255fdd37f96d763cb73e8f8e6e4778c8b228cbbc
Tobias Aagaard Christiansen <tach@trifork.com>
2026-04-14 10:06:14 +0200

    BankAccount class

---
+++ BankAccount.cs
@@ -0,0 +1,52 @@
+namespace BankProgram
+{
+    class BankAccount
+    {
+        private int id;
+        private double balance;
+        private string name;
+        private bool allowOverdraft;
+        private long overdraftLimit;
+
+        public BankAccount(int id, double balance, string name, bool allowOverdraft, long overdraftLimit)
+        {
+            this.id = id;
+            this.balance = balance;
+            this.name = name;
+            this.allowOverdraft = allowOverdraft;
+            this.overdraftLimit = overdraftLimit;
+        }
+
+        public string Name { get { return name; } set { name = value; }}
+
+        public string withdraw(double amount)
+        {
+            if (amount < 0 && allowOverdraft == false)
+            {
+                return "You cannot withdraw into a negative amount";
+            }
+            else if (amount < 0 && allowOverdraft == true && balance - amount < -overdraftLimit)
+            {
+                return $"You cannot exceed your limit of overdraft, which is {overdraftLimit}. Contact your bank for more information.";
+            }
+            else
+            {
+                balance -= amount;
+                return "Withdrawal was successfull, your balance is now: " + balance;
+            }
+        }
+
+        public string deposit(double amount)
+        {
+            if (amount < 0)
+            {
+                return "You cannot deposit a negative amount";
+            }
+            else
+            {
+                balance += amount;
+                return "Deposit was successfull, your balance is now: " + balance;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using BankProgram;
+
+namespace BankApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<BankAccount> accounts = new List<BankAccount>();
+            BankAccount account1 = new BankAccount(1, 2700, "Sørn Dam", true, 5000);
+            BankAccount account2 = new BankAccount(2, 500, "Mads Dam", false, 0);
+            BankAccount account3 = new BankAccount(3, 10000, "Mikkel Dam", true, 10000);
+            accounts.Add(account1);
+            accounts.Add(account2);
+            accounts.Add(account3);
+
+            Console.WriteLine(account1.withdraw(3000));
+        }
+    }
+}
